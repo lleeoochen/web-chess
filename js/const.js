8e84be1c6a980defcc3aec186014b5701a9a7e14
Wei Tung Chen <weitungchen1998@gmail.com>
2020-02-14 00:58:27 -0800

    css change

--- js/const.js
+++ js/const.js
@@ -5,8 +5,8 @@ const VALUE = {King: 200, Queen: 9, Rook: 5, Bishop: 3, Knight: 3, Pawn: 1, None
 const TEAM = {B: "B", W: "W", None:"N"}
 
 const BOARD_SIZE = 8;
-const GRID_SIZE_P = Util.vw2px(9);
-const OFFSET_MARGIN = Util.vw2px(9);
+const GRID_SIZE_P = Util.vw2px(10);
+const OFFSET_MARGIN = Util.vw2px(10);
 
 const OFFSET_X_P = CANVAS_LAYER ? CANVAS_LAYER.getBoundingClientRect().left + OFFSET_MARGIN : 0;
 const OFFSET_Y_P = CANVAS_LAYER ? CANVAS_LAYER.getBoundingClientRect().top + OFFSET_MARGIN : 0;
