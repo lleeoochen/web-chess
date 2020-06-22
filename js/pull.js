defc97f38fbf5d9dcc2feaf4614308be76de095e
Wei Tung Chen <weitungchen1998@gmail.com>
2020-06-21 17:41:27 -0700

    removed pull to refresh + secured mobile topbar

--- js/pull.js
+++ js/pull.js
@@ -1,13 +1,13 @@
 $(function() {
-	if (SCREEN_PORTRAIT) {
-		const ptr = PullToRefresh.init({
-			mainElement: 'body',
-			refreshTimeout: 100,
-			instructionsPullToRefresh: 'Refresh?',
-			instructionsReleaseToRefresh: 'Refresh?',
-			onRefresh() {
-				window.location.reload();
-			}
-		});
-	}
+	// if (SCREEN_PORTRAIT) {
+	// 	const ptr = PullToRefresh.init({
+	// 		mainElement: '#matches-display',
+	// 		refreshTimeout: 100,
+	// 		instructionsPullToRefresh: 'Refresh?',
+	// 		instructionsReleaseToRefresh: 'Refresh?',
+	// 		onRefresh() {
+	// 			window.location.reload();
+	// 		}
+	// 	});
+	// }
 });
