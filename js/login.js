e5f13cf6d72d5c7bf197ad7accac54a6b0d2b47e
Wei Tung Chen <weitungchen1998@gmail.com>
2020-06-22 21:40:07 -0700

    bug fix

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ database.login(() => {
 	let url = localStorage.getItem(LAST_VISITED_KEY);
 	localStorage.removeItem(LAST_VISITED_KEY);
 
-	if (url)
+	if (url && !url.includes('login'))
 		window.location = url;
 	else
 		window.location = `{{ baseUrl }}/${ SCREEN_PORTRAIT ? 'mobile' : '' }`;
